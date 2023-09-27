17bb878e964722297cf67954cc881688c285b6db
Bachir <bafosth@yahoo.fr>
2023-09-26 20:22:09 -0400

    created entity and dto packages and added the corresponding classes. also added the links to thml

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.cydeo.controller;
 
+import com.cydeo.dto.UserDTO;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,9 +11,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class UserController {
 
     @GetMapping("/create")
-    public String createUser() {
+    public String createUser(Model model) {
 
-//        model.addAttribute("user");
+        model.addAttribute("user", new UserDTO());
 
         return "user/create";
     }
--- src/main/java/com/cydeo/entity/User.java
+++ src/main/java/com/cydeo/entity/User.java
@@ -4,6 +4,8 @@ import com.cydeo.enums.Gender;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @NoArgsConstructor
 @Data
 public class User extends BaseEntity {
@@ -17,6 +19,18 @@ public class User extends BaseEntity {
     private Role role;
     private Gender gender;
 
-
-
+    // NOTE: when you are working with Lombok, and you are extending from another class or a super class,
+    // then lombok will not create a constructor including the super class fields.
+    // therefore, you need to manually crate the " all args constructor " as shown below
+    public User(Long id, LocalDateTime insertDateTime, Long insertUserId, LocalDateTime lastUpdateDateTime, Long lastUpdateUserId, String firstName, String lastName, String userName, String password, boolean enabled, String phone, Role role, Gender gender) {
+        super(id, insertDateTime, insertUserId, lastUpdateDateTime, lastUpdateUserId);
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.userName = userName;
+        this.password = password;
+        this.enabled = enabled;
+        this.phone = phone;
+        this.role = role;
+        this.gender = gender;
+    }
 }
