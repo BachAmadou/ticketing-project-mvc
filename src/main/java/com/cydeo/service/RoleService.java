b9fd21cf0bfa943a0ff665e1eb575d25d7cdb468
Bachir <bafosth@yahoo.fr>
2023-09-27 11:51:12 -0400

    created CrudService interface and impl package

--- src/main/java/com/cydeo/service/RoleService.java
+++ src/main/java/com/cydeo/service/RoleService.java
@@ -4,12 +4,13 @@ import com.cydeo.dto.RoleDTO;
 
 import java.util.List;
 
-public interface RoleService {
+public interface RoleService extends CrudService<RoleDTO, Long> {
 
-    RoleDTO save(RoleDTO role);
-    RoleDTO findById(Long id);
-    List<RoleDTO> findAll();
-
-    void delete(RoleDTO role);
-    void deleteById(Long id);
+    // USING GENERICS: the below code should be deleted, but Im keeping them in comment for review purposes.
+    // once it is deleted, this interface will extend CrudService
+//    RoleDTO save(RoleDTO role);
+//    RoleDTO findById(Long id);
+//    List<RoleDTO> findAll();
+//    void delete(RoleDTO role);
+//    void deleteById(Long id);
 }
--- src/main/java/com/cydeo/service/UserService.java
+++ src/main/java/com/cydeo/service/UserService.java
@@ -4,12 +4,14 @@ import com.cydeo.dto.UserDTO;
 
 import java.util.List;
 
-public interface UserService {
+public interface UserService extends CrudService<UserDTO, String> {
 
-    UserDTO save(UserDTO user);
-    UserDTO findById(String username);
-    List<UserDTO> findAll();
-    void delete(UserDTO user);
-    void deleteById(String username);
+    // USING GENERICS: the below code should be deleted, but Im keeping them in comment for review purposes.
+    // once it is deleted, this interface will extend CrudService
+//    UserDTO save(UserDTO user);
+//    UserDTO findById(String username);
+//    List<UserDTO> findAll();
+//    void delete(UserDTO user);
+//    void deleteById(String username);
 
 }
