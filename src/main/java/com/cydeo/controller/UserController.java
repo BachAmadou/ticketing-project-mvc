291f2958dbfe3b34a17a9b511fb2d30bac701edf
Bachir <bafosth@yahoo.fr>
2023-10-17 09:32:38 -0400

    created enum for Status, created ProjectDTO, created ProjectController

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -1,74 +1,3 @@
-//package com.cydeo.controller;
-//
-//import com.cydeo.dto.UserDTO;
-//import com.cydeo.service.RoleService;
-//import com.cydeo.service.UserService;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.*;
-//
-//@Controller
-//@RequestMapping("/user")
-//public class UserController {
-//
-//    RoleService roleService;
-//    UserService userService;
-//
-//    public UserController(RoleService roleService, UserService userService) {
-//        this.roleService = roleService;
-//        this.userService = userService;
-//    }
-//
-//    @GetMapping("/create")
-//    public String createUser(Model model) {
-//
-//        model.addAttribute("user", new UserDTO());
-//        model.addAttribute("roles", roleService.findAll());
-//        model.addAttribute("users", userService.findAll());
-//
-//        return "user/create";
-//    }
-//
-//    @PostMapping("/create")
-//    public String insertUser(@ModelAttribute("user")UserDTO user, Model model) {
-//
-//        model.addAttribute("user", new UserDTO());
-//        model.addAttribute("roles", roleService.findAll());
-//
-//        userService.save(user); // this will save the data in the Map before displaying on the page
-//
-//        return "redirect:user/create";
-//    }
-//
-//    @GetMapping("/update/{username}")
-//    public String editUser(@PathVariable("username") String username, Model model) {
-//
-//        model.addAttribute("user", userService.findById(username));
-//        model.addAttribute("roles", roleService.findAll());
-//        model.addAttribute("users", userService.findAll());
-//
-//        return "/user/update";
-//    }
-//
-//    @PostMapping("/update/{username}")
-//    public String updateUser(@PathVariable("username") String username, UserDTO user){
-//
-//        userService.update(user);
-//
-//        return "redirect:/user/create";
-//    }
-//
-//    @GetMapping("/delete/{username}")
-//    public String deleteUser(@PathVariable("username") String username) {
-//
-//        userService.deleteById(username);
-//
-//        return "redirect:/user/create";
-//    }
-//
-//}
-
-
 package com.cydeo.controller;
 
 import com.cydeo.dto.UserDTO;
@@ -102,7 +31,6 @@ public class UserController {
         return "/user/create";
     }
 
-
     @PostMapping("/create")
     public String insertUser(@ModelAttribute("user") UserDTO user, Model model){
 
@@ -119,7 +47,6 @@ public class UserController {
         model.addAttribute("roles",roleService.findAll());
         model.addAttribute("users",userService.findAll());
 
-
         return "/user/update";
 
     }
@@ -132,7 +59,6 @@ public class UserController {
         return "redirect:/user/create";
 
     }
-
 
     @GetMapping("/delete/{username}")
     public String deleteUser(@PathVariable("username") String username){
