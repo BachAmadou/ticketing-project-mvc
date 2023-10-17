1dcde81600482b838673afee0b3b0e1c9c8e015f
Bachir <bafosth@yahoo.fr>
2023-10-17 13:26:53 -0400

    added button implementation on project/create page

--- src/main/java/com/cydeo/controller/ProjectController.java
+++ src/main/java/com/cydeo/controller/ProjectController.java
@@ -7,6 +7,7 @@ import com.cydeo.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -36,6 +37,14 @@ public class ProjectController {
     public String insertProject(ProjectDTO project) {
 
         projectService.save(project);
+
+        return "redirect:/project/create";
+    }
+
+    @GetMapping("/delete/{projectcode}")
+    public String deleteProject(@PathVariable("projectcode") String projectcode) {
+
+        projectService.deleteById(projectcode);
 
         return "redirect:/project/create";
     }
