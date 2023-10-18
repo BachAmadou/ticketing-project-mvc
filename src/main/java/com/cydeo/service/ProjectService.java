e99ca96e3fd4a3f5fa316a923fb99252e731332e
Bachir <bafosth@yahoo.fr>
2023-10-18 11:15:13 -0400

    added thml text

--- src/main/java/com/cydeo/service/ProjectService.java
+++ src/main/java/com/cydeo/service/ProjectService.java
@@ -8,6 +8,5 @@ import java.util.List;
 public interface ProjectService extends CrudService<ProjectDTO, String> {
     void complete(ProjectDTO project);
     List<ProjectDTO> findAllNonCompletedProjects();
-
     List<ProjectDTO> getCountedListOfProjectDTO(UserDTO manager);
 }
